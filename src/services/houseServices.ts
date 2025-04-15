e73ebffd33fd0e167320551427ae54deda63eca1
Tristan Santos <tristan456_6@live.com>
2025-04-15 11:22:20 +0800

    fix build error

--- src/services/houseServices.ts
+++ src/services/houseServices.ts
@@ -9,7 +9,7 @@ import {
 } from "@/types/HouseTypes";
 import { PaginatedDataType } from "@/types/paginatedType";
 import { CollectionType } from "@/validations/collectionSchema";
-import { HouseSchemaType } from "@/validations/houseSchema";
+import { HouseSchemaType } from "@/validations/residentSchema";
 
 const getHouses = async ({
   page,
